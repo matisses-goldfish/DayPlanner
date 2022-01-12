37312522918b6d85493f5ec3eca2313f1018924b
matisses-goldfish <91293629+matisses-goldfish@users.noreply.github.com>
2022-01-12 16:12:05 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,16 @@
  
     $(document).ready(function () {
-        //The list of all varriables used: 
-        var $7am = $("#7am");
-        var $8am = $("#8am");
-        var $9am = $("#9am");
-        var $10am = $("#10am");
-        var $11am = $("#11am");
-        var $12pm = $("#12pm");
-        var $1pm = $("#1pm");
-        var $2pm = $("#2pm");
-        var $3pm = $("#3pm");
-        var $4pm = $("#4pm");
-        var $5pm = $("#5pm");
         var saveBtn = $(".saveBtn");
 
         //displays time in day, month, numerical day, year 
+        // sourced: https://momentjs.com/
         var today = moment().format('dddd MMMM do, YYYY');
         console.log(today)
         $("#currentDay").append(today);
 
         //take the time from moment, converts into hours, then, depending on the time of day,
         //applys them to the class to change their color
+       // format sourced from : https://github.com/cibellem/dayplanner/blob/master/assets/js/script.js
         var now = parseInt(moment().format('HH'));
         $(".description").each(function () {
             var name = parseInt($(this).attr("name"));
@@ -37,7 +27,7 @@
                 $(this).addClass("future")
             }
         })
-    
+    //source from: https://www.codegrepper.com/code-examples/javascript/how+to+save+to+local+storage+jquery
        saveBtn.on("click", function () {
             var hour =$(this).parent().attr(".hour");
             var description = $(this).siblings(".description").val();
